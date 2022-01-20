a23008bfd9bb2827399c6a9f6bc8d79386d5489c
Magda <magdalenafalkowska00@wp.pl>
2022-01-20 16:57:06 +0100

    Added displaying cars

--- components/CarDetails.js
+++ components/CarDetails.js
@@ -1,21 +1,27 @@
+import { Text, View, StyleSheet } from "react-native";
 
-import { Text, View, StyleSheet } from 'react-native';
-
-function CarDetails() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.content}>This is a DETAIL screen</Text>
-      </View>
-    );
+function CarDetails({ route, navigation }) {
+  const item = route.params.item;
+  console.log(item);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.content}>Brand: {item.brand}</Text>
+      <Text style={styles.content}>Model: {item.model}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    content: {
-        margin: 20,
-        fontSize: 18,
-    }
+  content: {
+    margin: 20,
+    fontSize: 18,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
 
-export {
-    CarDetails
-}
+export { CarDetails };
