d21376b59936576cb292aae27745f0978fa202f1
Magda <magdalenafalkowska00@wp.pl>
2022-01-20 18:24:51 +0100

    Added displaying booking items and tab navigation.

--- App.js
+++ App.js
@@ -1,20 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { CarList } from './components/CarList';
-import { CarDetails } from './components/CarDetails';
-import { LoginPage } from './components/LoginPage';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { CarList } from "./components/CarList";
+import { CarDetails } from "./components/CarDetails";
+import { LoginPage } from "./components/LoginPage";
+import { BookingList } from "./components/BookingList";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+const isLoggedIn = true;
+
+function Home() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="CarListScreen"
+        component={CarList}
+        options={{
+          title: "CarListScreen",
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = "car-sharp";
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "green",
+          tabBarInactiveTintColor: "gray",
+        }}
+      />
+
+      <Tab.Screen
+        name="BookingListScreen"
+        component={BookingList}
+        options={{
+          title: "BookingListScreen",
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = "calendar-sharp";
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "green",
+          tabBarInactiveTintColor: "gray",
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="LoginPageScreen" component={LoginPage} options={{ title: 'LoginPageScreen' }} />
-        <Stack.Screen name="CarListScreen" component={CarList} options={{ title: 'CarListScreen' }} />
-        <Stack.Screen name="CarDetailsScreen" component={CarDetails} options={{ title: 'CarDetailsScreen' }} />
+        {!isLoggedIn ? (
+        <>
+          <Stack.Screen
+            name="LoginPageScreen"
+            component={LoginPage}
+            options={{ title: "LoginPageScreen" }}
+          />
+        </>
+        ) : (
+        <>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="CarListScreen"
+            component={CarList}
+            options={{ title: "CarListScreen" }}
+          />
+          <Stack.Screen
+            name="CarDetailsScreen"
+            component={CarDetails}
+            options={{ title: "CarDetailsScreen" }}
+          />
+        </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -23,12 +86,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   content: {
     margin: 20,
     fontSize: 18,
-  }
+  },
 });
