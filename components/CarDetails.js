ddef2d6d935ea1ed05c7c82e9b6ca4dc8f03a578
Magda <magdalenafalkowska00@wp.pl>
2022-01-22 18:06:40 +0100

    Added gallery

--- components/CarDetails.js
+++ components/CarDetails.js
@@ -1,8 +1,16 @@
-import { Text, View, StyleSheet, Image, LogBox } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  LogBox,
+  Dimensions,
+} from "react-native";
 import { useEffect, useState } from "react";
 import AsyncStorage from "async-storage";
-import { getImage, BASE_URL } from '../logic/api'
-import Gallery from 'react-native-image-gallery';
+import { getImage, BASE_URL } from "../logic/api";
+import Gallery from "react-native-image-gallery";
+import Carousel, { Pagination } from "react-native-snap-carousel";
 
 function CarDetails({ route, navigation }) {
   //const [imgSource, setImgSource] = useState("https://placekitten.com/300/200");
@@ -11,35 +19,45 @@ function CarDetails({ route, navigation }) {
   console.log(item);
 
   useEffect(async () => {
-    LogBox.ignoreLogs(['Animated: `useNativeDriver`']);
+    LogBox.ignoreLogs(["Animated: `useNativeDriver`"]);
     const token = await AsyncStorage.getItem("@Carly:apiToken");
     getImage(item.id, token)
       .then((data) => {
         if (data.length > 0) {
           let list = [];
           data.forEach((item) => {
-            list.push("http://10.0.2.2:8080/images/"+item);
+            list.push("http://10.0.2.2:8080/images/" + item);
             console.log(item);
-          })
-          console.log(list)
+          });
+          console.log(list);
           setImgSources(list);
           //setImgSource("http://10.0.2.2:8080/images/" + data[0]);
         }
       })
       .catch((err) => console.log(err));
   }, []);
-
-  const imageList = imgSources.map((image) =>{
-    return({ source: { uri: image }, dimensions: { width: 150, height: 150 }  });
-  })
-
+  //{ id: '1', image: IMAGES.image1 }
+  const imageList = imgSources.map((image) => {
+    console.log({ id: "image", image: image });
+    return { id: "image", image: image };
+  });
+  const { width } = Dimensions.get("window");
+  console.log(imageList);
   return (
     <View style={styles.container}>
-      <Gallery
-        style={{ flex: 1, backgroundColor: 'transparent', marginTop: 50 }}
-        images={imageList}
-        useNativeDriver={true}
-      />
+      <Carousel
+          data={imageList}
+          sliderWidth={width}
+          itemWidth={width}
+          renderItem={({ item, index }) => (
+            <Image
+              key={index}
+              style={{ width: '100%', height: '100%' }}
+              resizeMode='contain'
+              source={{ uri: item.image }}
+            />
+          )}
+        />
       <Text style={styles.content}>Brand: {item.brand}</Text>
       <Text style={styles.content}>Model: {item.model}</Text>
       <Text style={styles.content}>Engine: {item.engine}</Text>
