5400053ec73e0a6c93edfeb35ab2f54e8c090a47
Magda <magdalenafalkowska00@wp.pl>
2022-01-22 16:44:37 +0100

    Added gallery

--- components/CarDetails.js
+++ components/CarDetails.js
@@ -1,27 +1,45 @@
-import { Text, View, StyleSheet, Image } from "react-native";
+import { Text, View, StyleSheet, Image, LogBox } from "react-native";
 import { useEffect, useState } from "react";
 import AsyncStorage from "async-storage";
 import { getImage, BASE_URL } from '../logic/api'
+import Gallery from 'react-native-image-gallery';
 
 function CarDetails({ route, navigation }) {
-  const [imgSource, setImgSource] = useState("https://placekitten.com/300/200");
+  //const [imgSource, setImgSource] = useState("https://placekitten.com/300/200");
+  const [imgSources, setImgSources] = useState([]);
   const item = route.params.item;
   console.log(item);
 
   useEffect(async () => {
+    LogBox.ignoreLogs(['Animated: `useNativeDriver`']);
     const token = await AsyncStorage.getItem("@Carly:apiToken");
     getImage(item.id, token)
       .then((data) => {
         if (data.length > 0) {
-          setImgSource(BASE_URL + '/images/' + data[0]);
+          let list = [];
+          data.forEach((item) => {
+            list.push("http://10.0.2.2:8080/images/"+item);
+            console.log(item);
+          })
+          console.log(list)
+          setImgSources(list);
+          //setImgSource("http://10.0.2.2:8080/images/" + data[0]);
         }
       })
       .catch((err) => console.log(err));
   }, []);
 
+  const imageList = imgSources.map((image) =>{
+    return({ source: { uri: image }, dimensions: { width: 150, height: 150 }  });
+  })
+
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={{ uri: imgSource }} />
+      <Gallery
+        style={{ flex: 1, backgroundColor: 'transparent', marginTop: 50 }}
+        images={imageList}
+        useNativeDriver={true}
+      />
       <Text style={styles.content}>Brand: {item.brand}</Text>
       <Text style={styles.content}>Model: {item.model}</Text>
       <Text style={styles.content}>Engine: {item.engine}</Text>
